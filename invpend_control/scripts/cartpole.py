6c548e725a212ba5360c0ad1b8d6c34b1e51a942
linzhank <linzhank@gmail.com>
2018-01-11 23:21:24 -0500

    minor change oncartpole

--- invpend_control/scripts/cartpole.py
+++ invpend_control/scripts/cartpole.py
@@ -16,7 +16,10 @@ from sensor_msgs.msg import JointState
 from std_srvs.srv import Empty
 from gazebo_msgs.msg import LinkState
 from geometry_msgs.msg import Point
-    
+
+def exceedRange(pos_cart, pos_pole):
+    return math.fabs(pos_cart) > 2.4 or math.fabs(pos_pole) > math.pi/6
+
 class CartPole(object):
     """ Testbed, for the pupose of testing cart-pole system """
     def __init__(self):
@@ -45,9 +48,8 @@ class CartPole(object):
     	self.pos_pole = data.position[0]
     	self.vel_pole = data.velocity[0]
         # For debug purpose, uncomment the following line
-        print("cart_position: {0:.5f}, cart_velocity: {1:.5f}, pole_angle: {2:.5f}, pole_angular_velocity: {3:.5f} \
-              # ".format(self.pos_cart, self.vel_cart, self.pos_pole, self.vel_pole))
-        self.ex_rng = self.exceedRange()
+        print("cart_position: {0:.5f}, cart_velocity: {1:.5f}, pole_angle: {2:.5f}, pole_angular_velocity: {3:.5f} ".format(self.pos_cart, self.vel_cart, self.pos_pole, self.vel_pole))
+        self.ex_rng = exceedRange(self.pos_cart, self.pos_pole)
         if self.ex_rng == True:
             reset_count = 0
             print("=== reset invpend pos ===\n")
@@ -61,10 +63,7 @@ class CartPole(object):
                 self.vel_cart = 0
                 self.pos_pole = 0
                 self.vel_pole = 0
-                
-    def exceedRange(self):
-        return math.fabs(self.pos_cart) > 2.4 or math.fabs(self.pos_pole) > math.pi/6
-        
+                        
     def take_action(self, vel_cmd):
         '''
         For the purpose of test, 
@@ -72,6 +71,7 @@ class CartPole(object):
         '''
         rate = rospy.Rate(self.freq)
         while not rospy.is_shutdown():
+            print(str(self.ex_rng)) # debug
             if self.ex_rng == True:
                 vel_cmd = 0
             self._pub_vel_cmd.publish(vel_cmd)
