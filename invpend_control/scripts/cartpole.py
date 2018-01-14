916f8c5f7e5bffdbeb4b09a27a87ad6317a3a100
linzhank <linzhank@gmail.com>
2018-01-14 15:48:52 -0500

    modifying qlearning, CartPole environment

--- invpend_control/scripts/cartpole.py
+++ invpend_control/scripts/cartpole.py
@@ -6,6 +6,7 @@
 
 from __future__ import print_function
 
+import numpy as np
 import math
 import random
 import time
@@ -18,7 +19,7 @@ from gazebo_msgs.msg import LinkState
 from geometry_msgs.msg import Point
 
 def exceedRange(pos_cart, pos_pole):
-    return math.fabs(pos_cart) > 2.4 or math.fabs(pos_pole) > math.pi/6
+    return math.fabs(pos_cart) > 2.4 or math.fabs(pos_pole) > math.pi/12 # cart: +-2.4; pole: +-15degrees
 
 class CartPole(object):
     """ Testbed, for the pupose of testing cart-pole system """
@@ -43,28 +44,37 @@ class CartPole(object):
         self.cmd = 0
 
     def jsCB(self, data):
+        """ Callback function for subscribing /invpend/joint_states topic """
     	rospy.loginfo("~~~Getting Inverted pendulum joint states~~~")
     	self.pos_cart = data.position[1]
     	self.vel_cart = data.velocity[1]
     	self.pos_pole = data.position[0]
     	self.vel_pole = data.velocity[0]
         # For debug purpose, uncomment the following line
-        print("cart_position: {0:.5f}, cart_velocity: {1:.5f}, pole_angle: {2:.5f}, pole_angular_velocity: {3:.5f} ".format(self.pos_cart, self.vel_cart, self.pos_pole, self.vel_pole))
+        # print("cart_position: {0:.5f}, cart_velocity: {1:.5f}, pole_angle: {2:.5f}, pole_angular_velocity: {3:.5f} ".format(self.pos_cart, self.vel_cart, self.pos_pole, self.vel_pole))
         self.ex_rng = exceedRange(self.pos_cart, self.pos_pole)
         if self.ex_rng == True:
-            reset_count = 0
-            print("=== reset invpend pos ===\n")
-            while reset_count < self.reset_dur*self.freq:
-                print("reset counter: ", str(reset_count)) # debug log
-                self._pub_vel_cmd.publish(0)
-                self._pub_set_pole.publish(self.PoleState)
-                reset_count += 1
-                rospy.sleep(1./self.freq)
-                self.pos_cart = 0
-                self.vel_cart = 0
-                self.pos_pole = 0
-                self.vel_pole = 0
-                        
+            self.resetEnv()
+
+    def resetEnv(self):
+        reset_count = 0
+        print("=== reset invpend pos ===\n")
+        while reset_count < self.reset_dur*self.freq:
+            print("reset counter: ", str(reset_count)) # debug log
+            self._pub_vel_cmd.publish(0)
+            self._pub_set_pole.publish(self.PoleState)
+            reset_count += 1
+            rospy.sleep(1./self.freq)
+            self.pos_cart = 0
+            self.vel_cart = 0
+            self.pos_pole = 0
+            self.vel_pole = 0
+            self.reward = 0
+                
+    def observeEnv(self):
+        """ Get cart-pole state, reward and out of range flag from environment """
+        return np.array([self.pos_cart, self.vel_cart, self.pos_pole, self.vel_pole]), self.reward, self.ex_rng
+
     def take_action(self, vel_cmd):
         '''
         For the purpose of test, 
--- invpend_control/scripts/cartpole_qtable.py
+++ invpend_control/scripts/cartpole_qtable.py
@@ -1,51 +1,132 @@
-learning_rate = get_learning_rate(0)
-explore_rate = get_explore_rate(0)
-discount_factor = 0.99  # since the world is unchanging
-num_streaks = 0
+#!/usr/bin/env python
+import numpy as np
+import math
+import random
+import time
 
-for episode in range(NUM_EPISODES):
-    # observe initial states
-    ob = observeEnv()
-    # convert observation to bucket (s1,s2,s3,s4,a)
-    state_0 = observeToBucket(ob)
-    # 
-    for step in range(MAX_STEP):
-        # select an action
-        action = select_action(state_0, explore_rate)
-        # execute the action, get new observation
-        ob, reward, done = take_action(action)
-        # convert new observation to bucket
-        state = state_to_bucket(ob)
-        # update Q-table based on new state
-        max_q = np.amax(q_table[state]) # max q-value
-        q_table[state_0 + (action,)] += learning_rate*(reward + discount_factor*max_q - q_table[state_0 + (action,)])
-        # reset state
-        state_0 = state
+import rospy
+from cartpole import CartPole
 
-        # debug, uncomment following prints
-        print("\nEpisode = %d" % episode)
-        print("step = %d" % step)
-        print("Action: %d" % action)
-        print("State: %s" % str(state))
-        print("Reward: %f" % reward)
-        print("Best Q: %f" % max_q)
-        print("Explore rate: %f" % explore_rate)
-        print("Learning rate: %f" % learning_rate)
-        print("Streaks: %d" % num_streaks)
+# Environment related constants
+ACTIONS = (-1, 0, 1) # discrete velocity command
+NUM_ACTIONS = len(ACTIONS)
+upper_bound = [2.4, 1, math.pi/6, math.radians(50)]
+lower_bound = [-2.4, -1, -math.pi/6, -math.radians(50)]
+STATE_BOUNDS = zip(lower_bound, upper_bound)
+NUM_BUCKETS = (3, 3, 6, 3) # (pos_cart, vel_cart, pos_pole, vel_pole)
+# Learning related constants
+MIN_LEARNING_RATE = 0.1
+MIN_EXPLORE_RATE = 0.01
+# Simulation related constans
+NUM_EPISODES = 1000
+MAX_STEP = 250
+STREAK_TO_END = 120
+SOLVED_STEP = 199
 
-        # finish current episode when max steps reached or cart-pole out of range
-        if done:
-            print("Episode %d finished after %f time steps" % (episode, step))
-            if step >= SOLVED_STEP:
-                num_srteaks += 1
-            else:
-                num_straks = 0
-            break
-    # finish learning when agent succeeded over certain times consequtively
-    if num_streaks > STREAK_TO_END:
-        break
+class QlearnCartPole(CartPole):
+    """ Inherent from CartPole Class and add q-learning method """
+    def __init__(self):
+        CartPole.__init__(self)
 
-    # update parameters
-    explore_rate = get_explore_rate(episode)
-    learning_rate = get_learning_rate(episode)
-            
+    def train(self):
+        learning_rate = get_learning_rate(0)
+        explore_rate = get_explore_rate(0)
+        discount_factor = 0.99  # since the world is unchanging
+        num_streaks = 0
+
+        # Q-table
+        q_table = np.zeros(NUM_BUCKETS + (NUM_ACTIONS,))
+
+        for episode in range(NUM_EPISODES):
+            # initialize environment
+            self.resetEnv()
+            # observe initial states, ([pos_cart, vel_cart, pos_pole, vel_pole])
+            ob, _, _ = self.observeEnv()
+            # find out observation's index in q_table (s1,s2,s3,s4,_)
+            state_0 = observeToBucket(ob)
+            # 
+            for step in range(MAX_STEP):
+                # select an action
+                action = select_action(state_0, explore_rate)
+                # execute the action, get new observation
+                self.take_action(action)
+                # time.sleep(1./50)
+                # observe env change
+                ob, reward, done = self.observeEnv()
+                # convert new observation to bucket
+                state = observeToBucket(ob)
+                # update Q-table based on new state
+                max_q = np.amax(q_table[state]) # max q-value
+                q_table[state_0 + (action,)] += learning_rate*(reward + discount_factor*max_q - q_table[state_0 + (action,)])
+                # reset state
+                state_0 = state
+
+                # debug, uncomment following prints
+                print("\nEpisode = %d" % episode)
+                print("step = %d" % step)
+                print("Action: %d" % action)
+                print("State: %s" % str(state))
+                print("Reward: %f" % reward)
+                print("Best Q: %f" % max_q)
+                print("Explore rate: %f" % explore_rate)
+                print("Learning rate: %f" % learning_rate)
+                print("Streaks: %d" % num_streaks)
+
+                # finish current episode when max steps reached or cart-pole out of range
+                if done:
+                    print("Episode %d finished after %f time steps" % (episode, step))
+                    if step >= SOLVED_STEP:
+                        num_srteaks += 1
+                    else:
+                        num_straks = 0
+                    break
+            # finish learning when agent succeeded over certain times consequtively
+            if num_streaks > STREAK_TO_END:
+                break
+
+                # update parameters
+                explore_rate = get_explore_rate(episode)
+                learning_rate = get_learning_rate(episode)
+
+def get_learning_rate(episode):
+    return max(MIN_LEARNING_RATE, min(0.5, 1.0-math.log10((t+1)/25)))
+
+def get_explore_rate(episode):
+    return max(MIN_EXPLORE_RATE, min(1, 1.0-math.log10((t+1)/25)))
+    
+def observeToBucket(state):
+    bucket_indice = []
+    for i in range(len(state)):
+        if state[i] <= STATE_BOUNDS[i][0]:
+            bucket_index = 0
+        elif state[i] >= STATE_BOUNDS[i][1]:
+            bucket_index = NUM_BUCKETS[i] - 1
+        else:
+            # Mapping the state bounds to the bucket array
+            bound_width = STATE_BOUNDS[i][1] - STATE_BOUNDS[i][0]
+            offset = (NUM_BUCKETS[i]-1)*STATE_BOUNDS[i][0]/bound_width
+            scaling = (NUM_BUCKETS[i]-1)/bound_width
+            bucket_index = int(round(scaling*state[i] - offset))
+        bucket_indice.append(bucket_index)
+    return tuple(bucket_indice)
+
+def select_action(state, explore_rate):
+    # Select a random action
+    if random.random() < explore_rate:
+        action = env.action_space.sample()
+    # Select the action with the highest q
+    else:
+        action = ACTIONS[np.argmax(q_table[state])]
+    return action
+
+def main():
+    """ Set up Q-learning and run """
+    print("Initiating simulation...")
+    rospy.init_node('q_learning')
+    ql_agent = CartPole()
+    rospy.onshutdown(ql_agent.clean_shutdown)
+    ql_agent.train()
+    rospy.spin()
+    
+if __name__ == '__main__':
+    main()
