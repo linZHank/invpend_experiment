9387d0fb2eaf25af08fdb01c07b0aaeded00c091
linzhank <linzhank@gmail.com>
2018-01-14 11:14:25 -0500

    remove out of date scripts

--- invpend_control/scripts/invpend_qnet.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
-%matplotlib inline
-
-tf.reset_default_graph()
-inputs1 = tf.placeholder(shape=[1,16],dtype=tf.float32)
-W = tf.Variable(tf.random_uniform([16,4,0,0,0.01]))
-
