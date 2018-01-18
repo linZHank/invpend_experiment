c69dec668766ebe815d9cbcc0f3ec66df7784a26
linzhank <linzhank@gmail.com>
2018-01-18 00:57:50 -0500

    work on qnet

--- invpend_control/scripts/qnet_train.py
+++ invpend_control/scripts/qnet_train.py
@@ -21,16 +21,13 @@ from cartpole import CartPole, bcolors
 # Time the code execution
 start_time = time.time()
 # Reinforement learning environment related settings
-## Discrete actions, states and buckets
-ACTIONS = (-5., 0., 5.) # discrete velocity command
+## Discrete actions
+ACTIONS = (-5., -4., -3., -2., -1., 0., 1., 2., 3., 4., 5.) # discrete velocity command
 NUM_ACTIONS = len(ACTIONS)
-upper_bound = [2.4, 1, math.pi/12, math.radians(50)]
-lower_bound = [-2.4, -1, -math.pi/12, -math.radians(50)]
-STATE_BOUNDS = zip(lower_bound, upper_bound)
-NUM_BUCKETS = (1, 1, 6, 3) # (pos_cart, vel_cart, pos_pole, vel_pole)
-## Learning related constants
-MIN_LEARNING_RATE = 0.1
-MIN_EXPLORE_RATE = 0.01
+## Neural Network settings
+INPUT_LAYER_SIZE = 4
+HIDDEN_LAYER_SIZE = 128
+OUTPUT_LAYER_SIZE = 9
 ## Simulation related constans
 NUM_EPISODES = 2000
 MAX_STEP = 250
@@ -42,11 +39,49 @@ class QlearnCartPole(CartPole):
         CartPole.__init__(self)
 
     def train(self):
-        # Initialize learning
-        input_state_node = tf.placeholder(tf.float32, shape=(1,4))
-        gamma = .99
-        epsilon = .1
-        # create oist to store total rewards and steps taken per epoch
+        # Traing settings
+        gamma = .99 # discount_factor
+        epsilon = .1 # explore_rate
+        # make space to store episodic reward accumulation
+        accumulated_reward = 0
+        reward_list = []
+        # neural network settings
+        ## The variables below hold all the trainable weights.
+        ## also try 1 layer model
+        states_node = tf.placeholder(dtype=tf.float32, shape=(1,INPUT_LAYER_SIZE))
+        qvalue_node = tf.placeholder(dtype=tf.float32, shape=(1,OUTPUT_LAYER_SIZE))
+        w1 = tf.Variable(tf.truncated_normal(shape=[INPUT_LAYER_SIZE, HIDDEN_LAYER_SIZE],
+                                             stddev = 0.01, dtype=tf.float32))
+        b1 = tf.Variable(tf.constant(value=0.01, shape=[HIDDEN_LAYER_SIZE], dtype=tf.float32))
+        w2 = tf.Variable(tf.truncated_normal(shape=[HIDDEN_LAYER_SIZE, OUTPUT_LAYER_SIZE],
+                                             stddev = 0.01, dtype=tf.float32))
+        ## define the neural network
+        def model(input_states):
+            hidden = tf.nn.relu(tf.matmul(data, w1) + tf.b1)
+            return tf.matmul(hidden, w2) + b2
+        ## output
+        q_values = model(states_node)
+        ## predictions, also try tf.nn.softmax(q_values)
+        prediction = tf.argmax(q_values)
+        # Loss function, also try tf.reduce_sum(tf.square(qvalue_node - q_values))
+        # loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+        #     labels=qvalue_node, logits=q_values))
+        loss = tf.reduce_sum(tf.square(qvalue_node - q_values))
+        # L2 regularization for the fully connected parameters.
+        regularizers = (tf.nn.l2_loss(w1) + tf.nn.l2_loss(b1) +
+                        tf.nn.l2_loss(w2) + tf.nn.l2_loss(b2))
+        ## (optional) add the regularization term to the loss, uncomment following line.
+        # loss += 5e-4 * regularizers
+        ## (optional) learning rate decaying, uncomment following lines
+        # learning_rate = tf.train.exponential_decay(
+        #     0.1,                # Base learning rate.
+        #     batch * BATCH_SIZE,  # Current index into the dataset.
+        #     train_size,          # Decay step.
+        #     0.95,                # Decay rate.
+        #     staircase=True)
+        ## optimizer, try MomentumOptimizer, AdamOptimizer, etc
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
+        
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             for epoch in range(num_epochs):
@@ -56,24 +91,44 @@ class QlearnCartPole(CartPole):
                 ob, _, _ = self.observe_env()
                 accumulated_reward = 0
                 while step < MAX_STEP and not out:
-                    action_index, q_value = sess.run([predict, q_output], feed_dict={input_state_node:np.array(ob)})
+                    action_index, Qvalue = sess.run([predict, q_output], feed_dict={input_state_node:np.array(ob)})
+                    # pick an action randomly by exploring
+                    if np.random.ran(1) < epsilon:
+                        action_index = random.randrange(0,NUM_ACTIONS)
+                    action = ACTIONS[action_index]
+                    # apply action
+                    self.take_action(action)
+                    # give environment some time to obtain new observation
+                    rate.sleep()
+                    # obtain new observation from environment
+                    ob, reward, out = self.observe_env()
+                    Qvalue_next = sess.run(q_output, feed_dict={input_state_node:np.array(ob)})
+                    maxQ_next = np.max(Qvalue_next)
+                    targetQ = Qvalue
+                    targetQ[0,]
+
+
+
+
+
+
+
+
+
+                    
+                    action = select_action(action_index, epsilon)
         # map joint states to slot in Q table
         state_0 = observeToBucket(ob)
-        # make space to store episodic reward accumulation
-        accumulated_reward = 0
-        reward_list = []
         # get ready to learn
         rate = rospy.Rate(self.freq)
         while not rospy.is_shutdown():
-            print(bcolors.OKBLUE, "::: Episode {0:d}, Step {1:d}".format(episode, step), bcolors.ENDC)
             # select an action with epsilon greedy, decaying explore_rate
             action_index, action = select_action(q_table, state_0, explore_rate)
             # apply action as velocity comand
             self.take_action(action)
             # give the enviroment some time to obtain new observation
             rate.sleep()
-            # obtain new observation from environment
-            ob, reward, out = self.observe_env()
+
             # map new observation to slot in Q table
             state = observeToBucket(ob)
             # update Q-table
@@ -82,6 +137,7 @@ class QlearnCartPole(CartPole):
             q_table[state_0 + (action_index,)] += learning_rate*(reward + discount_factor*max_q - q_table[state_0 + (action_index,)])
             if episode <= NUM_EPISODES and num_streaks < STREAK_TO_END:
                 if not out and step <= MAX_STEP:
+                    print(bcolors.OKBLUE, "::: Episode {0:d}, Step {1:d}".format(episode, step), bcolors.ENDC)
                     state_0 = state
                     accumulated_reward += 1
                     print("$$$ Accumulated reward in episode {0:d} was {1:d}".format(episode, accumulated_reward))
@@ -122,31 +178,9 @@ class QlearnCartPole(CartPole):
             rate.sleep()
 
 # Useful functions
-def get_learning_rate(episode):
-    return max(MIN_LEARNING_RATE, min(0.5, 1.0-math.log10((episode+1)/25.)))
-
-def get_explore_rate(episode):
-    return max(MIN_EXPLORE_RATE, min(1, 1.0-math.log10((episode+1)/25.)))
-    
-def observeToBucket(state):
-    bucket_indice = []
-    for i in range(len(state)):
-        if state[i] <= STATE_BOUNDS[i][0]:
-            bucket_index = 0
-        elif state[i] >= STATE_BOUNDS[i][1]:
-            bucket_index = NUM_BUCKETS[i] - 1
-        else:
-            # Mapping the state bounds to the bucket array
-            bound_width = STATE_BOUNDS[i][1] - STATE_BOUNDS[i][0]
-            offset = (NUM_BUCKETS[i]-1)*STATE_BOUNDS[i][0]/bound_width
-            scaling = (NUM_BUCKETS[i]-1)/bound_width
-            bucket_index = int(round(scaling*state[i] - offset))
-        bucket_indice.append(bucket_index)
-    return tuple(bucket_indice)
-
-def select_action(q_table, state, explore_rate):
+def select_action(action_index, epsilon):
     # Select a random action
-    if random.random() < explore_rate:
+    if random.random(1) < epsilon:
         act_idx = random.randrange(0,NUM_ACTIONS)
         action = ACTIONS[act_idx]
         print("!!! Action selected randomly !!!")
